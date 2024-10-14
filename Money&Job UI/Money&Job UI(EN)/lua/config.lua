764575584bb0db1b5fa1c615eeeeabb877dc2ec1
CocoScript-Lab <himerock2632@gmail.com>
2024-10-14 17:57:35 +0900

    Add files via upload

---
+++ Money&Job UI/Money&Job UI(EN)/lua/config.lua
@@ -0,0 +1,8 @@
+Config = {}
+
+Config.ShowCash = true           -- Show or hide the cash box
+Config.ShowBank = true           -- Show or hide the bank box
+Config.ShowBlackMoney = true     -- Show or hide the black money box (the item name for black money should be set as "black_money")
+Config.ShowJob = true            -- Show or hide the job box
+Config.ShowRank = true           -- Show or hide the job rank box
+Config.ShowPlayerID = true       -- Show or hide the player ID box
